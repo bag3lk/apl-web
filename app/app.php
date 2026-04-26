dc145988d2625176a0ab2872fed60c0652f4f44c
bag3lk <czemujalol@outlook.com>
2026-04-26 16:31:44 +0200

    struktura

---
+++ app/app.php
@@ -0,0 +1,12 @@
+<div id="parent">
+    <div id="logowanie">
+        <h1>Zaloguj się do aplikacji</h1>
+        <!-- ekran logowania -->
+            <?php
+                
+            ?>
+        <hr>
+        <h5> lub zarejestruj się! </h5>
+        <!-- rejestracja -->
+    </div>
+</div>
